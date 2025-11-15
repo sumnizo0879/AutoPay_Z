ea04aff06904fef343ab319fceac4b75958027ed
sumnizo0879 <sumnizo0879@users.noreply.github.com>
2025-11-15 14:41:37 +0000

    Add subscriptions-factory.rs, types.less, App.css

---
+++ frontend/web/src/analytics-api-report/static/subscriptions-factory.rs
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/autopay_z/frontend/web/src/analytics-api-report/static/subscriptions-factory.rs
+// Generated at: 2025-12-03T23:45:44.444Z
+
