3cdabf95daae6b099968b92266857dbdf9642cb0
sumnizo0879 <sumnizo0879@users.noreply.github.com>
2025-11-22 12:40:21 +0000

    Add local-base-global.css, local.js, secure-configuration.py

---
+++ local-frontend/local.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+
+export const fragments_build_fragments = () => { return null; };
+
---
+++ local-frontend/secure-configuration.py
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/autopay_z/local-frontend/secure-configuration.py
+// Generated at: 2025-12-03T23:45:44.442Z
+
